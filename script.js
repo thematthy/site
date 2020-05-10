534fd69a4b0db5b7b0aa47c632863f8a791f1337
thematthy <51312843+thematthy@users.noreply.github.com>
2020-05-10 15:19:23 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const startingMinutes = 47.4;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+   const minutes = Math.floor(time / 60);
+   let seconds = time % 60;
+   
+   seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+
+   countdownEl.innerHTML = `${minutes}:${seconds}`;
+   time !== 0 ? time-- : time;
+}
+
+if (seconds < 10) {
+  greeting = 'Good morning';
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
